616c82f2a1e95a012e1471a9afed78dddc380658
Abhishek <user@DESKTOP-ASH69UE>
2023-07-26 12:33:13 +0530

    abhishek

---
+++ src/pages/CMS/Cms.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Sidebar from '../../components/sidebar/Sidebar';
+import Navbar from '../../components/navbar/Navbar';
+
+export const Cms = () => {
+  return (
+    <div>
+   <div className="home">
+      <Sidebar className='nav' style={{}} />
+      <div className="homeContainer">
+      <Navbar />
+
+      </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,88 @@
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+export default function Login() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+  const handleClick =()=>{
+    <Link to='/'>
+      Dashboard
+    </Link>
+  }
+
+  return (
+    <Container component="main" maxWidth="sm">
+      <Box
+        sx={{
+          boxShadow: 3,
+          borderRadius: 2,
+          px: 4,
+          py: 6,
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form>
+        <Box  noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+           <Link to="/cms" style={{ textDecoration: "none" }}>
+          <Button onClick={handleClick}
+          
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+          </Link>
+          <Grid container>
+           
+           
+          </Grid>
+        </Box>
+        </form>
+      </Box>
+    </Container>
+  );
+}
