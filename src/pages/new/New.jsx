65a080507acbb6841a8c08dd552924e55091821f
tanusinghh <40310712+tanusinghh@users.noreply.github.com>
2023-07-31 14:44:05 +0530

    tan newuser

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -38,7 +38,7 @@ const New = ({ inputs, title }) => {
             <div className="container">
               <div className="row d-flex justify-content-center align-items-center h-100">
                 <div className="col-lg-12 col-xl-11">
-                  <div className="card mt-5 mb-5 text-black" style={{ borderRadius: "25px" }}>
+                  <div className="card mt-5 mb-5 text-black" style={{ borderRadius: "25px",border: "none" }}>
                     <div className="card-body p-md-5">
                       <div className="row justify-content-center">
                         <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
@@ -94,7 +94,7 @@ const New = ({ inputs, title }) => {
                             </div>
 
                           </form>
-{/* hello */}
+
                         </div>
                         <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
                           <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
