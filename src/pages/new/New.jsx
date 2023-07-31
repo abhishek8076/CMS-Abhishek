44a6dac92e45c3b8a19bb1567e3d2e575aea9741
tanusinghh <40310712+tanusinghh@users.noreply.github.com>
2023-07-31 13:23:06 +0530

    hello

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -94,7 +94,7 @@ const New = ({ inputs, title }) => {
                             </div>
 
                           </form>
-
+{/* hello */}
                         </div>
                         <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
                           <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
