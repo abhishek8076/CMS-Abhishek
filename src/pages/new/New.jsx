3148e5dc7c9320c25c77cfec72791cd4483da20f
tanusinghh <40310712+tanusinghh@users.noreply.github.com>
2023-07-31 12:50:07 +0530

    tanu newuser 3107

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -30,82 +30,87 @@ const New = ({ inputs, title }) => {
 
   return (
     <>   <div>
-    <div className="home">
-       <Sidebar className='nav' style={{}} />
-       <div className="homeContainer">
-       <Navbar />
-       <section style={{backgroundColor: '#eee'}}>
-  <div className="container">
-    <div className="row d-flex justify-content-center align-items-center h-100">
-      <div className="col-lg-12 col-xl-11">
-        <div className="card mt-5 mb-5 text-black" style={{borderRadius: "25px"}}>
-          <div className="card-body p-md-5">
-            <div className="row justify-content-center">
-              <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+      <div className="home">
+        <Sidebar className='nav' style={{}} />
+        <div className="homeContainer">
+          <Navbar />
+          <section style={{ backgroundColor: '#eee' }}>
+            <div className="container">
+              <div className="row d-flex justify-content-center align-items-center h-100">
+                <div className="col-lg-12 col-xl-11">
+                  <div className="card mt-5 mb-5 text-black" style={{ borderRadius: "25px" }}>
+                    <div className="card-body p-md-5">
+                      <div className="row justify-content-center">
+                        <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
 
-                <form className="mx-1 mx-md-4">
+                          <form className="mx-1 mx-md-4">
 
-                  <div className="d-flex flex-row align-items-center mb-4">
-                    <i className="fas fa-user fa-lg me-3 fa-fw"></i>
-                    <div className="form-outline flex-fill mb-0">
-                      <label className="form-label" for="form3Example1c">Username</label>
-                      <input type="text" id="form3Example1c" className="form-control" />
+                            <div className="d-flex flex-row align-items-center mb-4">
+                              <i className="fas fa-user fa-lg me-3 fa-fw"></i>
+                              <div className="form-outline flex-fill mb-0">
+                                <label className="form-label" for="form3Example1c">Username</label>
+                                <input type="text" id="form3Example1c" className="form-control" style={{border : '2px solid black'}}/>
+                              </div>
+                            </div>
+
+                            <div className="d-flex flex-row align-items-center mb-4">
+                              <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                              <div className="form-outline flex-fill mb-0">
+                                <label className="form-label" for="form3Example3c">Email</label>
+                                <input type="email" id="form3Example3c" className="form-control" style={{border : '2px solid black'}} />
+                              </div>
+                            </div>
+
+                            <div className="d-flex flex-row align-items-center mb-4">
+                              <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
+                              <div className="form-outline flex-fill mb-0">
+                                <label for="usertype">Usertype</label><br></br>
+                                <select id="usertype" name="usertype" style={{ width: '22vw',backgroundColor : "white", border : '2px solid black', padding : '6px', borderRadius:'8px', marginTop : '4px'}}>
+                                  <option value="Select">Select</option>
+                                  <option value="Superadmin">Superadmin</option>
+                                  <option value="Admin">Admin</option>
+                                  
+                                </select>
+                              </div>
+                            </div>
+
+                            <div className="d-flex flex-row align-items-center mb-4">
+                              <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
+                              <div className="form-outline flex-fill mb-0">
+                                <label className="form-label" for="form3Example4c">Mobile No</label>
+                                <input type="password" id="form3Example4c" className="form-control" style={{border : '2px solid black'}} />
+                              </div>
+                            </div>
+                            <div className="d-flex flex-row align-items-center mb-4">
+                              <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
+                              <div className="form-outline flex-fill mb-0">
+                                <label className="form-label" for="form3Example4c">Address</label>
+                                <input type="password" id="form3Example4c" className="form-control" style={{border : '2px solid black'}} />
+                              </div>
+                            </div>
+
+                            <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                              <button type="button" className="btn btn-primary btn-lg">Register</button>
+                            </div>
+
+                          </form>
+
+                        </div>
+                        <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+                          <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
+                            className="img-fluid" alt="Sample image" />
+                        </div>
+                      </div>
                     </div>
                   </div>
-
-                  <div className="d-flex flex-row align-items-center mb-4">
-                    <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                    <div className="form-outline flex-fill mb-0">
-                      <label className="form-label" for="form3Example3c">Email</label>
-                      <input type="email" id="form3Example3c" className="form-control" />
-                    </div>
-                  </div>
-
-                  <div className="d-flex flex-row align-items-center mb-4">
-                    <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
-                    <div className="form-outline flex-fill mb-0">
-                        <label className="form-label" for="form3Example4c">Usertype</label>
-                        <input type="password" id="form3Example4c" className="form-control" />
-                    </div>
-                  </div>
-
-                  <div className="d-flex flex-row align-items-center mb-4">
-                    <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
-                    <div className="form-outline flex-fill mb-0">
-                      <label className="form-label" for="form3Example4c">Mobile No</label>
-                      <input type="password" id="form3Example4c" className="form-control" />
-                    </div>
-                  </div>
-                  <div className="d-flex flex-row align-items-center mb-4">
-                    <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
-                    <div className="form-outline flex-fill mb-0">
-                         <label className="form-label" for="form3Example4c">Address</label>
-                         <input type="password" id="form3Example4c" className="form-control" />
-                    </div>
-                  </div>
-
-                  <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                    <button type="button" className="btn btn-primary btn-lg">Register</button>
-                  </div>
-
-                </form>
-
-              </div>
-              <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
-                  className="img-fluid" alt="Sample image"/>
+                </div>
               </div>
             </div>
-          </div>
+          </section>
         </div>
       </div>
     </div>
-  </div>
-</section>
-       </div>
-       </div>
-     </div>
-    
+
     </>
   );
 };
