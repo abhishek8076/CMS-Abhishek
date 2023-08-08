e320926b2439fd858171d8f0da96733d1be9be08
tanusinghh <40310712+tanusinghh@users.noreply.github.com>
2023-08-08 14:47:18 +0530

    TANY

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,53 +1,39 @@
-import "./new.scss";
-import axios from 'axios';
+import React, { useState } from 'react';
+import axios from 'axios'
+import { Col, Button, Row, Container, Card, Form } from 'react-bootstrap';
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
-import { useState } from "react";
-// import 'bootstrap/dist/css/bootstrap. css';
-import Box from '@mui/material/Box';
-// import TextField from '@mui/material/TextField';
-import IconButton from '@mui/material/IconButton';
-import Input from '@mui/material/Input';
-import FilledInput from '@mui/material/FilledInput';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import InputLabel from '@mui/material/InputLabel';
-import InputAdornment from '@mui/material/InputAdornment';
-import FormHelperText from '@mui/material/FormHelperText';                       
-import FormControl from '@mui/material/FormControl';
-import TextField from '@mui/material/TextField';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import {
-  Container,
-  Grid,
-  Card,
-  CardHeader,
-  CardContent,
-  Divider
-} from '@mui/material';
-
-import Button from '@mui/material/Button';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-// import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
-// import MobileDatePicker from '/packages/x-date-pickers/src/MobileDatePicker'
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Link, useNavigate } from 'react-router-dom';
-import api from "../../utils/apiUrl.json"
+import 'bootstrap/dist/css/bootstrap.css';
+import Select from 'react-select';
+import api from '../../utils/apiUrl.json';
+import { Options } from 'react-select';
+import MenuItem from '@mui/material/MenuItem';
 
 
-const New = () => {
+export function New() {
+  const [age, setAge] = React.useState('');
+  const roles = [
+    { label: 'Admin', value: "1" },
+    { label: 'DEO', value: "2" }
+  ]
+  const [selectedRole, setSelectedRole] = useState('')
+  const handleRoleChange = (event) => {
+    setSelectedRole(event.target.value); // Update the state with the selected value
+  };
+
+
+  const handleChange1 = (event) => {
+    setAge(event.target.value);
+  };
   const [formData, setFormData] = useState({
     name: '',
     email: '',
     mobile_no: '',
-    address:'',
-    usertype:'',
-    createdby:'',
-    password:'',
-    ip_address:''
+    address: '',
+    usertype: '',
+    createdby: 'admin',
+    password: '',
+    ip_address: '192.168.0.1'
   });
 
   const handleChange = (event) => {
@@ -69,9 +55,14 @@ const New = () => {
 
         // Optionally, reset the form after successful submission
         setFormData({
-         name: '',
+          name: '',
           email: '',
-          mobile_no: ''
+          mobile_no: '',
+          address: '',
+          usertype: '',
+          createdby: 'admin',
+          password: '',
+          ip_address: '192.168.0.1'
         });
       })
       .catch((error) => {
@@ -82,95 +73,120 @@ const New = () => {
 
 
   return (
-    <>   <div>
-      <div className="home">
-        <Sidebar className='nav' style={{}} />
-        <div className="homeContainer">
-          <Navbar />             
-          <div>
-      <div className="container">
-        <div className="row mt-5">
-          <div className="col-9 offset-2">
-            <Card>
-              <CardHeader title="Registration Form" />
-              <form onSubmit={handleSubmit}>
-              <CardContent>
-                <FormControl  sx={{ m: 1 }} variant="standard">
-                  <InputLabel htmlFor="standard-adornment-amount">Name</InputLabel>
-                  <Input
-                    required
-                    name="name"
-                    value={formData.name}
-                    onChange={handleChange}
+    <>
+      <div>
+        <div className="home">
+          <Sidebar className='nav' style={{}} />
+          <div className="homeContainer">
+            <Navbar />
+            <div></div>
+            <Container>
 
-                  />
-                </FormControl>
-               
-           
+              <Row className="vh-100 d-flex justify-content-center align-items-center">
 
-             
-                <FormControl fullWidth sx={{ m: 1 }} variant="standard">
-                  <InputLabel htmlFor="standard-adornment-amount">Email</InputLabel>
-                  <Input
-                    required
-                    type="email"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                  />
-                </FormControl>
-                <FormControl fullWidth sx={{ m: 1 }} variant="standard">
-                  <InputLabel htmlFor="standard-adornment-amount">mobile</InputLabel>
-                  <Input
-                    required
-                    type="text"
-                    name="mobile"
-                    value={formData.mobile}
-                    onChange={handleChange}
-                  />
-                </FormControl>
-                <FormControl fullWidth sx={{ m: 1 }} variant="standard">
-                  <InputLabel htmlFor="standard-adornment-amount">Password</InputLabel>
-                  <Input
-                    required
-                    type="password"
-                    name="password"
-                    value={formData.password}
-                    onChange={handleChange}
-                  />
-                </FormControl>
-                <FormControl fullWidth sx={{ m: 1 }} variant="standard">
-                  <InputLabel htmlFor="standard-adornment-amount">Address</InputLabel>
-                  <Input
-                    required
-                    type="text"
-                    name="address"
-                    value={formData.address}
-                    onChange={handleChange}
-                  />
-                </FormControl>
-                <Box sx={{ '& button': { m: 3 } }}>
-                  <div>
-                    <Button  type="submit" variant="contained">Submit</Button>
-                  </div>
-                </Box>
-              </CardContent>
-              </form>
-              <div>
-                
-              </div>
-            </Card>
+                <Col md={10} lg={6} xs={12}>
+
+                  <Card.Body>
+                    <div className="mb-3 mt-md-4">
+                      <h2 className="fw-bold mb-4 text-center text-uppercase ">
+                        User Details
+                      </h2>
+                      <div className="mb-3">
+
+                        <Form onSubmit={handleSubmit}>
+                          <Form.Group className="mb-3" controlId="Name">
+                            <Form.Label className="text-center">Name</Form.Label>
+                            <Form.Control type="text"
+                              placeholder="Enter Name"
+                              name="name"
+                              value={formData.name}
+                              onChange={handleChange} />
+                          </Form.Group>
+
+                          <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label className="text-center">
+                              Email
+                            </Form.Label>
+                            <Form.Control type="email" placeholder="Enter email"
+
+                              name="email"
+                              value={formData.email}
+                              onChange={handleChange}
+                            />
+                          </Form.Group>
+
+                          <Form.Group className="mb-3" controlId="Password">
+                            <Form.Label className="text-center">Password</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Password"
+                              name="password"
+                              value={formData.password}
+                              onChange={handleChange} />
+                          </Form.Group>
+
+                          <Form.Group className="mb-3" controlId="Mobile">
+                            <Form.Label className="text-center">Mobile</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Mobile No"
+                              name="mobile_no"
+                              value={formData.mobile_no}
+                              onChange={handleChange} />
+                          </Form.Group>
+
+                          <Form.Group className="mb-3" controlId="Address">
+                            <Form.Label className="text-center">Address</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Address"
+                              name="address"
+                              value={formData.address}
+                              onChange={handleChange} />
+
+                          </Form.Group>
+
+                          {/* <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            
+                            name="usertype"
+                            label="User Type"
+                            onChange={handleChange}
+                          >
+                            <MenuItem value={formData.usertype} >Ten</MenuItem>
+                            <MenuItem value={formData.usertype} >Twenty</MenuItem>
+                            <MenuItem value={formData.usertype} >Thirty</MenuItem>
+                          </Select> */}
+                          <Form.Group className="mb-3" controlId="Address">
+                            <div className="mb-12"><Form.Label className="text-center">Role</Form.Label>
+                              <select className='form-control' name='usertype' value={selectedRole} onChange={handleRoleChange}>
+                                <option value={formData.usertype}>Select a role</option>
+                                {roles.map((role) => (
+                                  <option key={role.value} value={formData.usertype}>
+                                    {role.label}
+                                  </option>
+                                ))}
+                              </select>
+
+                            </div>
+                          </Form.Group>
+                          <Form.Group
+                            className="mb-3"
+                            controlId="formBasicCheckbox"
+                          ></Form.Group>
+                          <div id="button" className=" d-flex">
+                            <Button variant="primary" type="submit" style={{ width: 100 }}>
+                              Submit
+                            </Button>
+
+                          </div>
+                        </Form>
+                      </div>
+                    </div>
+                  </Card.Body>
+
+                </Col>
+              </Row>
+            </Container>
           </div>
         </div>
-      </div >
-   
-    </div >
-        </div>
       </div>
-    </div>
-
     </>
   );
-};
-
+}
 export default New;
