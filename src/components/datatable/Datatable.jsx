a6dde0862b343383dc55ee51d89580224e117465
Abhishek <user@DESKTOP-ASH69UE>
2023-09-05 12:40:58 +0530

    abhishek

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -1,33 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress'; // Import CircularProgress for the loader
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import AddIcon from '@mui/icons-material/Add';
-import api from '../../utils/apiUrl.json'
-import './datatable.scss'
+import api from '../../utils/apiUrl.json';
+import './datatable.scss';
 
-import { Table,
-   TableBody, 
-   TableCell,
-    TableContainer,
-     TableHead,
-      TableRow, 
-      Paper,
-     } from '@mui/material';
-     import apiClient from "../../services/AxiosApi";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from '@mui/material';
+import apiClient from '../../services/AxiosApi';
 
 const Datatable = () => {
   const [data, setData] = useState([]);
-  //  const [catId, setCatId] = useState(null);
+  const [loading, setLoading] = useState(true); // State variable to control the loader visibility
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await apiClient.get(api.newuser);
         setData(response.data);
+        setLoading(false); // Set loading to false when data is fetched
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -35,71 +38,67 @@ const Datatable = () => {
     fetchData();
   }, []);
 
-    const handleButtonClick = (users_id) => {
-      // Find the item with the clicked ID
-      const selectedItem = data.find(item => item.users_id === users_id);
-      
-      if (selectedItem) {
-        // Do something with the selected item data
-        console.log('Selected item:', selectedItem);
-      }
-    };
+  const handleButtonClick = (users_id) => {
+    const selectedItem = data.find((item) => item.users_id === users_id);
 
-// debugger;
+    if (selectedItem) {
+      console.log('Selected item:', selectedItem);
+    }
+  };
+
   return (
     <>
-    
-          <div className="datatable">
-      <div className="datatableTitle"style={{paddingTop: 20, paddingLeft: 10}}>
-        <h2>Add New User</h2>
-        <Link to="/users/new" className="link">
-        <Button  id="btn" variant="contained"><AddIcon/>Add New</Button>
-        </Link>
-      </div>
+      <div className="datatable">
+        <div className="datatableTitle" style={{ paddingTop: 20, paddingLeft: 10 }}>
+          <h2>Add New User</h2>
+          <Link to="/users/new" className="link">
+            <Button id="btn" variant="contained">
+              <AddIcon /> Add New
+            </Button>
+          </Link>
+        </div>
       </div>
       <div className="container">
+        {loading ? ( // Display loader when loading is true
+          <CircularProgress style={{ margin: '20px auto', display: 'block' }} />
+        ) : (
           <TableContainer component={Paper} className="table">
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-          <TableCell>ID</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Email</TableCell>
-            <TableCell>Mobile No</TableCell>
-            <TableCell>Address</TableCell>
-            {/* <TableCell>Address</TableCell> */}
-            <TableCell>Action</TableCell>
-            {/* <TableCell>Delete</TableCell> */}
-            {/* Add more table header cells for other properties */}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map((item,i) => (
-            
-            <TableRow key={item.users_id}>
-               <TableCell>{i+1}</TableCell>
-              <TableCell>{item.user_name}</TableCell>
-              <TableCell>{item.user_email}</TableCell>
-              <TableCell>{item.user_mobile_no}</TableCell>
-              <TableCell>{item.user_address}</TableCell>
-              <TableCell> <Link Link to={`/users/single/${item.users_id}`} style={{ textDecoration: "none" }}>
-        <Button variant="contained"  onClick={() => handleButtonClick(item.user_id)}><EditIcon/></Button>
-        </Link>
-        <Button variant="contained"  onClick={() => handleButtonClick(item.user_id)}><DeleteIcon/></Button>
-        {/* <Link to="/users/new" className="link"></Link>
-        <Button variant="outlined" >Delete</Button> */}
-        </TableCell>
-              
-              
-              {/* <TableCell>{item.name}</TableCell> */}
-              
-              {/* Add more table cells for other properties */}
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-    </div>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>ID</TableCell>
+                  <TableCell>Name</TableCell>
+                  <TableCell>Email</TableCell>
+                  <TableCell>Mobile No</TableCell>
+                  <TableCell>Address</TableCell>
+                  <TableCell>Action</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {data.map((item, i) => (
+                  <TableRow key={item.users_id}>
+                    <TableCell>{i + 1}</TableCell>
+                    <TableCell>{item.user_name}</TableCell>
+                    <TableCell>{item.user_email}</TableCell>
+                    <TableCell>{item.user_mobile_no}</TableCell>
+                    <TableCell>{item.user_address}</TableCell>
+                    <TableCell>
+                      <Link Link to={`/users/single/${item.users_id}`} style={{ textDecoration: 'none' }}>
+                        <Button variant="contained" onClick={() => handleButtonClick(item.user_id)}>
+                          <EditIcon />
+                        </Button>
+                      </Link>
+                      <Button variant="contained" onClick={() => handleButtonClick(item.user_id)}>
+                        <DeleteIcon />
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        )}
+      </div>
     </>
   );
 };
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Container from "@mui/material/Container";
@@ -20,20 +20,20 @@ export default function Login() {
     r_password: ""
   });
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-const jsonData= { // Adjust the endpoint
-  r_email: user.email,
-  r_password: user.password
-}
-const handleSubmit = async (event) => {
+  const jsonData = { // Adjust the endpoint
+    r_email: user.email,
+    r_password: user.password
+  }
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
-    const response = await apiClient.post(api.login ,jsonData);
+    const response = await apiClient.post(api.login, jsonData);
 
     // if (response.status === 200  && response.email === response.data.r_email)
     //    {
     //   const  token  = response.data.token;
     //   const  data = response.data.user
-      
+
     //   if (token) {
     //     localStorage.setItem("token",token );
     //     localStorage.setItem("userData", data); // Assuming your user data is in `response.data.user`
@@ -48,26 +48,29 @@ const handleSubmit = async (event) => {
     // }
     if (response && response.data) {
       if (response.status == 200) {
-          let dt = response.data;
-          let user = dt.user;
-          let token = dt.token
-          localStorage.setItem("user",  JSON.stringify(user))
-          const storedUserString = localStorage.getItem('user');
-          const u = JSON.parse(storedUserString)
-         
-          if (dt) {
-            localStorage.setItem("token", token)
-              alert("login Sucess!!")
-              navigate("/dashboard")
-          }
-      }
-      else if (response.data.status== 401) {
-          alert("UserName or Password Not Matched")
-      }
+        let dt = response.data;
+        let user = dt.user;
+        let token = dt.token
+        localStorage.setItem("user", JSON.stringify(user))
+        const storedUserString = localStorage.getItem('user');
+        const u = JSON.parse(storedUserString)
+
+        if (dt) {
+          localStorage.setItem("token", token)
+          alert("login Sucess!!")
+          navigate("/dashboard")
+        }
+     
+
+    }
   }
-  else {
+  else if (response.r_email != jsonData.r_email ||
+    response.r_password != jsonData.r_password) {
+    alert("UserName  and Password Not Matched")
+  }
+    else {
       alert("Unauthorised User")
-  }
+    }
   };
 
   const handleChange = (event) => {
@@ -76,69 +79,69 @@ const handleSubmit = async (event) => {
       ...prevUser,
       [name]: value
     }));
-    
-  };
-console.log(user);
-let u =localStorage.getItem('user');
-console.log(u);
 
-// const storedUserString = localStorage.getItem('user');
-// const storedUser = JSON.parse(storedUserString)
-// console.log(localStorage.setItem('user', JSON.stringify(use)))
-// console.log(storedUser);
-useEffect(() => {
-  localStorage.clear();
-}, []);
+  };
+  console.log(user);
+  let u = localStorage.getItem('user');
+  console.log(u);
+
+  // const storedUserString = localStorage.getItem('user');
+  // const storedUser = JSON.parse(storedUserString)
+  // console.log(localStorage.setItem('user', JSON.stringify(use)))
+  // console.log(storedUser);
+  useEffect(() => {
+    localStorage.clear();
+  }, []);
   return (
-    <>  
+    <>
       <Container component="main" maxWidth="sm">
-      <Box
-        sx={{
-          boxShadow: 3,
-          borderRadius: 2,
-          px: 4,
-          py: 6,
-          marginTop: 8,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <img src={logo}/>
-        <Typography component="h1" variant="h5">
-         
-        </Typography>
-        <form onSubmit={handleSubmit}>
-          <TextField
-            margin="normal"
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-            onChange={handleChange}
-          />
-          <TextField
-            margin="normal"
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            onChange={handleChange}
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Sign In
-          </Button>
-        </form>
-      </Box>
-      
-    </Container>
+        <Box
+          sx={{
+            boxShadow: 3,
+            borderRadius: 2,
+            px: 4,
+            py: 6,
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <img src={logo} />
+          <Typography component="h1" variant="h5">
+
+          </Typography>
+          <form onSubmit={handleSubmit}>
+            <TextField
+              margin="normal"
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              onChange={handleChange}
+            />
+            <TextField
+              margin="normal"
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              onChange={handleChange}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+          </form>
+        </Box>
+
+      </Container>
 
     </>
   );
