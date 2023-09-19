502995a0b81230a25018398cd0313f9bbc7d7308
Abhishek <user@DESKTOP-ASH69UE>
2023-09-19 13:27:23 +0530

    demo

--- src/datatablesource.js
+++ src/datatablesource.js
@@ -150,53 +150,97 @@ export const option2 = [
   },
   
 ];
+//  export const Menuoptions = [
+//   {
+//     id: 1,
+//     "name": "Home",
+//     "is_submenu": false,
+//     "menu_id": null
+//   },
+//   {
+//     id: 2,
+//     "name": "Products",
+//     "is_submenu": true,
+//     "menu_id": 1
+//   },
+//   {
+//     id: 3,
+//     "name": "About Us",
+//     "is_submenu": false,
+//     "menu_id": null
+//   },
+//   {
+//     id: 4,
+//     "name": "Services",
+//     "is_submenu": true,
+//     "menu_id": 1
+//   },
+//   {
+//     id: 5,
+//     "name": "Services1",
+//     "is_submenu": true,
+//     "menu_id": 4
+//   },
+//   {
+//     id: 6,
+//     "name": "Services2",
+//     "is_submenu": true,
+//     "menu_id": 4
+//   },
+//   {
+//     id: 7,
+//     "name": "Home1",
+//     "is_submenu": true,
+//     "menu_id": 1
+//   },
+//   {
+//     id: 8,
+//     "name": "Aboutus1",
+//     "is_submenu": true,
+//     "menu_id": 3
+//   }
+// ]
  export const Menuoptions = [
   {
     id: 1,
-    "name": "Home",
-    "is_submenu": false,
-    "menu_id": null
+    name: "Home",
+    has_submenu: 1,
+    is_submenu: 0,
+    menu_id: null,
+    content_id: 1, // Content ID for this item
+    content_type: "link", // Content type (e.g., "link", "file", "html")
+    content_data: "https://example.com", // Content data (URL, file path, HTML code, etc.)
   },
   {
     id: 2,
-    "name": "Products",
-    "is_submenu": true,
-    "menu_id": 1
+    name: "Products",
+    has_submenu: 0,
+    is_submenu: 0,
+    menu_id: null,
+    content_id: 2, // Content ID for this item
+    content_type: "file", // Content type (e.g., "link", "file", "html")
+    content_data: "/path/to/pdf.pdf", // Content data (URL, file path, HTML code, etc.)
   },
   {
     id: 3,
-    "name": "About Us",
-    "is_submenu": false,
-    "menu_id": null
+    name: "About Us",
+    has_submenu: 1,
+    is_submenu: 0,
+    menu_id: null,
+    content_id: 3, // Content ID for this item
+    content_type: "html", // Content type (e.g., "link", "file", "html")
+    content_data: "<h1> About Us HTML Content</h1>", // Content data (URL, file path, HTML code, etc.)
   },
   {
     id: 4,
-    "name": "Services",
-    "is_submenu": true,
-    "menu_id": 1
+    name: "About Us1",
+    has_submenu: 1,
+    is_submenu: 1,
+    menu_id: 3,
+    content_id: 3, // Content ID for this item
+    content_type: "html", // Content type (e.g., "link", "file", "html")
+    content_data: "<h1> my name is abhshek</h1>", // Content data (URL, file path, HTML code, etc.)
   },
-  {
-    id: 5,
-    "name": "Services1",
-    "is_submenu": true,
-    "menu_id": 4
-  },
-  {
-    id: 6,
-    "name": "Services2",
-    "is_submenu": true,
-    "menu_id": 4
-  },
-  {
-    id: 7,
-    "name": "Home1",
-    "is_submenu": true,
-    "menu_id": 1
-  },
-  {
-    id: 8,
-    "name": "Aboutus1",
-    "is_submenu": true,
-    "menu_id": 3
-  }
-]
+  
+  // ... continue with other menu items
+];
--- src/pages/cmsDisplay/CmsDisplay.jsx
+++ src/pages/cmsDisplay/CmsDisplay.jsx
@@ -4,143 +4,96 @@ const Menuoptions = [
   {
     id: 1,
     name: "Home",
-    is_submenu: false,
+    has_submenu: 1,
+    is_submenu: 0,
     menu_id: null,
+    content_id: 1, // Content ID for this item
+    content_type: "link", // Content type (e.g., "link", "file", "html")
+    content_data: "https://example.com", // Content data (URL, file path, HTML code, etc.)
   },
   {
     id: 2,
     name: "Products",
-    is_submenu: false,
+    has_submenu: 0,
+    is_submenu: 0,
     menu_id: null,
+    content_id: 2, // Content ID for this item
+    content_type: "file", // Content type (e.g., "link", "file", "html")
+    content_data: "/path/to/pdf.pdf", // Content data (URL, file path, HTML code, etc.)
   },
   {
     id: 3,
     name: "About Us",
-    is_submenu: false,
+    has_submenu: 1,
+    is_submenu: 0,
     menu_id: null,
+    content_id: 3, // Content ID for this item
+    content_type: "html", // Content type (e.g., "link", "file", "html")
+    content_data: "<h1> About Us HTML Content</h1>", // Content data (URL, file path, HTML code, etc.)
   },
   {
     id: 4,
-    name: "Services",
-    is_submenu: false,
-    menu_id: null,
-  },
-  {
-    id: 5,
-    name: "Services1",
-    is_submenu: true,
-    menu_id: 4,
-  },
-  {
-    id: 6,
-    name: "Services2",
-    is_submenu: true,
-    menu_id: 4,
-  },
-  {
-    id: 7,
-    name: "Home1",
-    is_submenu: true,
-    menu_id: 1,
-  },
-  {
-    id: 8,
-    name: "Aboutus1",
-    is_submenu: true,
+    name: "About Us1",
+    has_submenu: 1,
+    is_submenu: 1,
     menu_id: 3,
+    content_id: 3, // Content ID for this item
+    content_type: "html", // Content type (e.g., "link", "file", "html")
+    content_data: "<h1> my</h1>", // Content data (URL, file path, HTML code, etc.)
   },
-  {
-    id: 9,
-    name: "Categories",
-    is_submenu: true,
-    menu_id: 2,
-  },
-  {
-    id: 10,
-    name: "Jackets",
-    is_submenu: true,
-    menu_id: 9,
-  },
-  {
-    id: 11,
-    name: "Tee",
-    is_submenu: true,
-    menu_id: 9,
-  },
-  {
-    id: 12,
-    name: "Jacket1",
-    is_submenu: true,
-    menu_id: 10,
-  },
-  {
-    id: 13,
-    name: "Jacket2",
-    is_submenu: true,
-    menu_id: 10,
-  },
-  {
-    id: 14,
-    name: "Tee1",
-    is_submenu: true,
-    menu_id: 11,
-  },
-  {
-    id: 15,
-    name: "Tee2",
-    is_submenu: true,
-    menu_id: 11,
-  },
-  {
-    id: 16,
-    name: "jeans",
-    is_submenu: true,
-    menu_id: 11,
-  },
+  
+  // ... continue with other menu items
 ];
 
+const Submenu = ({ items }) => {
+  return (
+    <ul>
+      {items.map((item) => (
+        <li key={item.id}>
+          <a href="#">{item.name}</a>
+          {item.has_submenu === 1 && <Submenu items={getSubmenuItems(item.id)} />}
+          <div>
+            {getContent(item.content_type, item.content_data)}
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const getSubmenuItems = (menuId) => {
+  return Menuoptions.filter((item) => item.menu_id === menuId && item.is_submenu === 1);
+};
+
+const getContent = (contentType, contentData) => {
+  if (contentType === "link") {
+    return <a href={contentData}>Link</a>;
+  } else if (contentType === "file") {
+    return (
+      <div>
+        <p>File Content:</p>
+        <a href={contentData} download>Download File</a>
+      </div>
+    );
+  } else if (contentType === "html") {
+    return <div dangerouslySetInnerHTML={{ __html: contentData }} />;
+  }
+};
+
 export const CmsDisplay = () => {
-  // Create a function to filter submenu items based on menu_id
-  const getSubmenuItems = (menuId) => {
-    return Menuoptions.filter((item) => item.menu_id === menuId);
-  };
+  const topLevelItems = Menuoptions.filter((item) => item.menu_id === null && item.is_submenu === 0);
 
   return (
-    <>
-      <ul>
-        {Menuoptions.map((item) => {
-          if (!item.is_submenu) {
-            const submenuItems = getSubmenuItems(item.id);
-            return (
-              <li key={item.id}>
-                <a href="#">{item.name}</a>
-                {submenuItems.length > 0 && (
-                  <ul>
-                    {submenuItems.map((submenuItem) => (
-                      <li key={submenuItem.id}>
-                        <a href="#">{submenuItem.name}</a>
-                        {/* Check for sub-submenu items */}
-                        {submenuItem.is_submenu && (
-                          <ul>
-                            {getSubmenuItems(submenuItem.id).map(
-                              (subSubmenuItem) => (
-                                <li key={subSubmenuItem.id}>
-                                  <a href="#">{subSubmenuItem.name}</a>
-                                </li>
-                              )
-                            )}
-                          </ul>
-                        )}
-                      </li>
-                    ))}
-                  </ul>
-                )}
-              </li>
-            );
-          }
-          return null;
-        })}
-      </ul>
-    </>
+    <ul>
+      {topLevelItems.map((item) => (
+        <li key={item.id}>
+          <a href="#">{item.name}</a>
+          {item.has_submenu === 1 && <Submenu items={getSubmenuItems(item.id)} />}
+          <div>
+            {getContent(item.content_type, item.content_data)}
+          </div>
+        </li>
+      ))}
+    </ul>
   );
 };
