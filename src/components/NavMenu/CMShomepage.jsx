5224bce64ffcfea4d97dd0fc7a04f04a12741f4e
Abhishek <user@DESKTOP-ASH69UE>
2023-10-04 16:13:27 +0530

    hjkjhjh

--- src/components/NavMenu/CMShomepage.jsx
+++ src/components/NavMenu/CMShomepage.jsx
@@ -6,6 +6,7 @@ import FroalaEditorComponent from 'react-froala-wysiwyg';
 // import CKEditor from '@ckeditor/ckeditor5-react';
 import { CKEditor } from 'ckeditor4-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { HtmlEditor } from '../htmlEditor/htmlEditor';
 // import '@ckeditor/ckeditor5-build-classic/build/translations/en-gb.css';
 
 export const CMShomepage = () => {
@@ -15,7 +16,19 @@ export const CMShomepage = () => {
         <h1>Home Page</h1>
         <div className="App">
             <h2>Using CKEditor 4 in React</h2>
-            
+            {/* <CKEditor
+        editor={ClassicEditor}
+        data={editorData}
+        onInit={(editor) => {
+          // You can customize the editor here
+          // editor.ui.view.editable.element.style.height = '300px';
+        }}
+        onChange={handleEditorChange}
+      /> */}
+      <CKEditor
+		type="inline"
+		initData={<p>This is a CKEditor 4 WYSIWYG editor instance created by ️⚛️ React.</p>}
+	/>,
         </div>
     </div>
   )
--- src/components/htmlEditor/htmlEditor.jsx
+++ src/components/htmlEditor/htmlEditor.jsx
@@ -1,5 +1,9 @@
 import React, { useRef } from 'react';
 import { Editor } from '@tinymce/tinymce-react';
+import {CKEditor} from "ckeditor4-react";
+
+CKEditor.editorUrl =
+  "https://cdnjs.cloudflare.com/ajax/libs/ckeditor/4.5.4/ckeditor.js";
 
 export const HtmlEditor=()=> {
     const editorRef = useRef(null);
@@ -10,24 +14,63 @@ export const HtmlEditor=()=> {
   };
   return (
     <>
-    <Editor
-        onInit={(evt, editor) => editorRef.current = editor}
-        initialValue="<p>This is the initial content of the editor.</p>"
-        init={{
-        height: 500,
-        menubar: false,
-        plugins: [
-           'a11ychecker','advlist','advcode','advtable','autolink','checklist','export',
-           'lists','link','image','charmap','preview','anchor','searchreplace','visualblocks',
-           'powerpaste','fullscreen','formatpainter','insertdatetime','media','table','help','wordcount'
-        ],
-        toolbar: 'undo redo | casechange blocks | bold italic backcolor | ' +
-           'alignleft aligncenter alignright alignjustify | ' +
-           'bullist numlist checklist outdent indent | removeformat | a11ycheck code table help',
-        content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+    <h1>jlkjsdlf</h1>
+    
+    <CKEditor
+        data="<p>Hello from CKEditor 4!</p>"
+        config={{
+          toolbar: [
+            { name: "tools", items: ["Maximize"] },
+            {
+              name: "clipboard",
+              items: ["Cut", "Copy", "Paste", "PasteText", "-", "Undo", "Redo"]
+            },
+            { name: "links", items: ["Link", "Unlink"] },
+            { name: "document", items: ["Source"] },
+            "/",
+            {
+              name: "basicstyles",
+              items: [
+                "Bold",
+                "Italic",
+                "Underline",
+                "Strike",
+                "-",
+                "Subscript",
+                "Superscript"
+              ]
+            },
+            {
+              name: "paragraph",
+              items: [
+                "NumberedList",
+                "BulletedList",
+                "-",
+                "Outdent",
+                "Indent",
+                "Blockquote"
+              ]
+            },
+            {
+              name: "align",
+              items: [
+                "AlignLeft",
+                "JustifyLeft",
+                "JustifyCenter",
+                "JustifyRight",
+                "JustifyBlock"
+              ]
+            },
+            "/",
+            { name: "styles", items: ["Format", "-", "Font", "-", "FontSize"] },
+            { name: "colors", items: ["TextColor", "BGColor"] },
+            { name: "insert", items: ["Image", "Table", "HorizontalRule"] },
+            "/"
+          ],
+          extraPlugins: "colorbutton,colordialog,font",
+          removeButtons: ""
         }}
-    />
-    <button onClick={log}>Log editor content</button>
+      />
     </>
   );
 }
