7ba6f5bb11f822abb433fa9441345b0838f1870c
Abhishek <user@DESKTOP-ASH69UE>
2023-10-21 17:42:09 +0530

    gjhkjl

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from '@mui/material/Button';
 import CircularProgress from '@mui/material/CircularProgress';
 import EditIcon from '@mui/icons-material/Edit';
@@ -10,7 +9,6 @@ import AddIcon from '@mui/icons-material/Add';
 import api from '../../utils/apiUrl.json';
 import './datatable.scss';
 import HomeIcon from '@mui/icons-material/Home';
-
 
 import {
   Table,
@@ -24,7 +22,8 @@ import {
   DialogTitle,
   DialogContent,
   DialogActions,
-  TextField, // Import TextField for filtering
+  TextField,
+  TablePagination,
 } from '@mui/material';
 import apiClient from '../../services/AxiosApi';
 
@@ -33,10 +32,9 @@ const Datatable = () => {
   const [loading, setLoading] = useState(true);
   const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
   const [deleteItemId, setDeleteItemId] = useState(null);
-  const [mobileFilter, setMobileFilter] = useState('');
-  // Filter state for name and email
-  const [nameFilter, setNameFilter] = useState('');
-  const [emailFilter, setEmailFilter] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -66,14 +64,11 @@ const Datatable = () => {
 
   const handleDeleteConfirm = async () => {
     try {
-      // Make a DELETE request to your API with the deleteItemId
       await apiClient.delete(api.deleteuser + deleteItemId);
 
-      // Remove the deleted item from the data array
       const updatedData = data.filter((item) => item.users_id !== deleteItemId);
       setData(updatedData);
 
-      // Close the dialog
       setOpenDeleteDialog(false);
     } catch (error) {
       console.error('Error deleting item:', error);
@@ -85,114 +80,98 @@ const Datatable = () => {
   };
 
   const filteredData = data.filter((item) => {
-    const lowerCaseName = item.user_name.toLowerCase();
-    const lowerCaseEmail = item.user_email.toLowerCase();
-    const lowerCaseMobile = item.user_mobile_no.toLowerCase();
-
-    return (
-      (lowerCaseName.includes(nameFilter.toLowerCase()) ||
-        lowerCaseEmail.includes(nameFilter.toLowerCase()) ||
-        lowerCaseMobile.includes(nameFilter.toLowerCase()))
-    );
+    const lowerCaseSearchTerm = searchTerm.toLowerCase();
+    return Object.values(item).some((value) => {
+      if (typeof value === 'string') {
+        return value.toLowerCase().includes(lowerCaseSearchTerm);
+      }
+      return false;
+    });
   });
 
-  return (
-    <>
-      <div className="datatable">
-        <div className="datatableTitle" style={{ paddingTop: 20, paddingLeft: 10 }}>
-          <h2>Add New User</h2>
-          <Link to="/users/new" className="link">
-            <Button id="btn" variant="contained">
-              <AddIcon /> Add New
-            </Button>
-          </Link>
-        </div>
-        <div className="datatableTitle" style={{ paddingTop: 20, paddingLeft: 10 }}>
-         
-          <Link to="/dashboard" className="link">
-            <Button id="btn" variant="contained">
-            <HomeIcon /> Back
-            </Button>
-          </Link>
-        </div>
+  const emptyRows = rowsPerPage - Math.min(rowsPerPage, filteredData.length - page * rowsPerPage);
 
-        {/* Filter inputs */}
-        {/* <TextField
+  return (
+    <div className="datatable-container">
+      <div className="datatable-header">
+        <h2>Add New User</h2>
+        <Link to="/users/new" className="link">
+          <Button variant="contained" startIcon={<AddIcon />}>
+            Add New
+          </Button>
+        </Link>
+      </div>
+      <div className="datatable-header">
+        <Link to="/dashboard" className="link">
+          <Button variant="contained" startIcon={<HomeIcon />}>
+            Back
+          </Button>
+        </Link>
+      </div>
+      <div className="search-bar">
+        <TextField
           label="Search"
           variant="outlined"
-          value={nameFilter || emailFilter || mobileFilter}
-          onChange={(e) => {
-            const searchValue = e.target.value.toLowerCase();
-            setNameFilter(searchValue);
-            setEmailFilter(searchValue);
-            setMobileFilter(searchValue);
-          }}
-        /> */}
-
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          maxRows={6}
+        />
       </div>
-      <div className="container">
-        {loading ? (
-          <CircularProgress style={{ margin: '20px auto', display: 'block' }} />
-        ) : (
-          <TableContainer component={Paper} className="table" style={{ maxHeight: '500px', overflow: 'auto' }}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableHead>
-                <TableRow>  <TextField
-                  label="Search"
-                  variant="outlined"
-                  value={nameFilter || emailFilter || mobileFilter}
-                  onChange={(e) => {
-                    const searchValue = e.target.value.toLowerCase();
-                    setNameFilter(searchValue);
-                    setEmailFilter(searchValue);
-                    setMobileFilter(searchValue);
-                  }}
-                />
-                </TableRow>
-                {/* <Link to="/dashboard">
-                  <Button variant="primary" type="submit" style={{ width: 100 }}>
-                    Back
-                  </Button>
-                </Link> */}
-
-                <TableRow>
-                  <TableCell>ID</TableCell>
-                  <TableCell>Name</TableCell>
-                  <TableCell>Email</TableCell>
-                  <TableCell>Mobile No</TableCell>
-                  <TableCell>Address</TableCell>
-                  <TableCell>Action</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {filteredData.map((item, i) => (
-                  <TableRow key={item.users_id}>
-                    <TableCell>{i + 1}</TableCell>
-                    <TableCell>{item.user_name}</TableCell>
-                    <TableCell>{item.user_email}</TableCell>
-                    <TableCell>{item.user_mobile_no}</TableCell>
-                    <TableCell>{item.user_address}</TableCell>
-                    <TableCell>
-                      <Link
-                        to={`/users/single/${item.users_id}`}
-                        style={{ textDecoration: 'none' }}
-                      >
-                        <Button variant="contained" onClick={() => handleButtonClick(item.user_id)}>
-                          <EditIcon />
-                        </Button>
-                      </Link>
-                      <Button variant="contained" onClick={() => handleDeleteClick(item.users_id)}>
-                        <DeleteIcon />
+      {loading ? (
+        <div className="loading">
+          <CircularProgress />
+        </div>
+      ) : (
+        <TableContainer component={Paper} className="table">
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell>Name</TableCell>
+                <TableCell>Email</TableCell>
+                <TableCell>Mobile No</TableCell>
+                <TableCell>Address</TableCell>
+                <TableCell>Action</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {filteredData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((item, i) => (
+                <TableRow key={item.users_id}>
+                  <TableCell>{i + 1}</TableCell>
+                  <TableCell>{item.user_name}</TableCell>
+                  <TableCell>{item.user_email}</TableCell>
+                  <TableCell>{item.user_mobile_no}</TableCell>
+                  <TableCell>{item.user_address}</TableCell>
+                  <TableCell>
+                    <Link to={`/users/single/${item.users_id}`} className="link">
+                      <Button variant="contained" onClick={() => handleButtonClick(item.user_id)} startIcon={<EditIcon />}>
+                        Edit
                       </Button>
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        )}
-  
-      </div>
+                    </Link>
+                    <Button variant="contained" onClick={() => handleDeleteClick(item.users_id)} startIcon={<DeleteIcon />}>
+                      Delete
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: 53 * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+          <TablePagination
+            rowsPerPageOptions={[10]}
+            component="div"
+            count={filteredData.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={(event, newPage) => setPage(newPage)}
+            onRowsPerPageChange={() => {}}
+          />
+        </TableContainer>
+      )}
       <Dialog open={openDeleteDialog} onClose={handleDeleteCancel}>
         <DialogTitle>Confirm Delete</DialogTitle>
         <DialogContent>
@@ -207,7 +186,7 @@ const Datatable = () => {
           </Button>
         </DialogActions>
       </Dialog>
-    </>
+    </div>
   );
 };
 
